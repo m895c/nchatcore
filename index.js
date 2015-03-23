d919a1cf856dec4ff08375f47ba1bf30f4a18520
m895c <menon.ananth@gmail.com>
2015-03-24 02:51:47 +0530

    Base Chat
    Basic chat and client id recog

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//app.get('/', function(req, res){
+//  res.send('<h1>NCORE HOST TEST </h1>  INIT ON:--*nCORe e92834n2323naskj32');
+//});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+    socket.on('chat message', function(msg){
+
+      io.emit('chat message', msg);
+      socket.broadcast.emit("chat message", socket.client.id);
+
+  });
+});
+
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *nCORe e92834n2323naskj32:3000');
+});
