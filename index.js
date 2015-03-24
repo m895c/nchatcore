ce1121342a74cbe99e7685036691706855717ff3
m895c <menon.ananth@gmail.com>
2015-03-25 01:46:13 +0530

    My sql db added
    USER Raw data and lat known tables created

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'xcfc54123',
+  database : 'nchat'
+});
 
 //app.get('/', function(req, res){
 //  res.send('<h1>NCORE HOST TEST </h1>  INIT ON:--*nCORe e92834n2323naskj32');
@@ -15,19 +24,70 @@ app.get('/css/main.css', function(req, res){
   res.sendFile(__dirname + '/css/main.css');
 });
 
+
+//SOCKET CONNECTION FROM CLIENT
 io.on('connection', function(socket){
 
+//CHAT MESSAGE FROM CLIENT
     socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
 
-    io.emit('chat message', msg);
-    socket.broadcast.emit("chat message", socket.client.id);
+//CLIENT SENDS INFO MESSAGE WITH USER:
+    socket.on('info', function(msg){
+      io.emit('chat message', msg);
 
-  });
+      var info = {name: msg.name,
+                  age: msg.age,
+                  sex: msg.sex,
+                  target: msg.target,
+                  token: msg.token,
+                  online: 1,
+                  sockid: socket.client.id
+                 };
+
+      connection.query('INSERT IGNORE INTO users SET ?',info,function(err, result) {
+        if (err) throw err;
+
+        console.log("User inserted users :" + result.insertId);
+
+      });
+
+
+      connection.query('INSERT IGNORE INTO lastknownuser SET ?',info,function(err, result) {
+        if (err) throw err;
+
+        console.log("User inserted lastknown :" + result.insertId);
+
+      });
+    });
+
+
+//CLIENT DISCONNECTS
+    socket.on('disconnect', function(){
+      io.emit('chat message', "disconnected");
+      connection.query('DELETE FROM lastknownuser WHERE sockid = ?', [socket.client.id], function(err, result) {
+        if (err) throw err;
+
+        console.log("USER OFFLINE : " + result.id);
+      });
+
+      connection.query('UPDATE users SET ? WHERE sockid = ?', [{online: 0}, socket.client.id], function(err, result) {
+        if (err) throw err;
+
+        console.log("USER OFFLINE : " + result.id);
+      });
+
+
+    });
+
+
 });
 
-// GET
 
 
+
+//EXPRESS SERVER START.
 http.listen(3000, function(){
   console.log('listening on *nCORe e92834n2323naskj32:3000');
 });
