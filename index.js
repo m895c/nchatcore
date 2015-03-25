3af6358c9ad3af25e733c8cb6c135748084218f8
m895c <menon.ananth@gmail.com>
2015-03-26 01:20:16 +0530

    basic Matching and search and matched messages

--- index.js
+++ index.js
@@ -62,7 +62,35 @@ io.on('connection', function(socket){
       });
     });
 
+//CLIENT SENDS SEARCH
+    socket.on('search', function(msg){
+      var search = {name: msg.name,
+                    age: msg.age,
+                    sex: msg.sex,
+                    target: msg.target,
+                    token: msg.token,
+                    sockid: socket.client.id
+                    };
 
+      //pseudocode
+      connection.query('SELECT sockid FROM lastknownuser where sex = ? ORDER BY sockid ASC LIMIT 1', [msg.target], function(err, result) {
+        if (err) throw err;
+
+        socket.room = search.token;
+
+        console.log("the search object is " + search.token);
+        console.log("the result is" + result[0]);
+
+        io.to(search.sockid).emit('matched', "room self: " + socket.room);
+
+        io.to(result[0].sockid).emit('matched', "room target: " + socket.room);
+
+
+        console.log("USER FOUND : " + result[0].sockid);
+                    });
+
+
+    });
 //CLIENT DISCONNECTS
     socket.on('disconnect', function(){
       io.emit('chat message', "disconnected");
