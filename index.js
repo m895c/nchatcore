fcad4157a9c8e903e3dc0d72a455a563f9561dd5
m895c <menon.ananth@gmail.com>
2015-03-28 02:09:09 +0530

    graceful exit and additions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'xcfc54123',
+    password: 'bull5I!97!',
     database: 'nchat'
 });
 //Default Route
@@ -16,9 +16,8 @@ app.get('/', function(req, res) {
 app.get('/css/main.css', function(req, res) {
     res.sendFile(__dirname + '/css/main.css');
 });
-//SOCKET CONNECTION FROM CLIENT
+//SOCKET CONNECTION FROM CLIENT 
 io.on('connection', function(socket) {
-   
     //CLIENT SENDS INFO MESSAGE WITH USER:
     socket.on('info', function(msg) {
         io.emit('infomessage', msg);
@@ -61,56 +60,73 @@ io.on('connection', function(socket) {
             console.log("the result is" + result[0].sockid);
             if (result[0] === null) {
                 io.to(search.sockid).emit('nomatch', "60");
-            } else  
-
-
-            //socket.client(result[0].sockid).join(search.token);
-
-            io.to(search.sockid).emit('matched', result[0].sockid);
-
-            //io.to(search.sockid).emit('chat message', "SELF: " + search.sockid );
-
-            io.to(result[0].sockid).emit('matched', result[0].sockid);
-
-           // io.to(result[0].sockid).emit('chat message',"HOST " + result[0].sockid + "R" );
-
-            socket.join(result[0].sockid);
-
-            console.log("USER FOUND : " + result[0].sockid);
-            setTimeout(function () {
-            console.log('Time is up');
-            socket.leave(result[0].sockid);
-            },25000);
-
+            } else {
+                //socket.client(result[0].sockid).join(search.token);
+                io.to(search.sockid).emit('matched', result[0].sockid);
+                //io.to(search.sockid).emit('chat message', "SELF: " + search.sockid );
+                io.to(result[0].sockid).emit('matched', result[0].sockid);
+                // io.to(result[0].sockid).emit('chat message',"HOST " + result[0].sockid + "R" );
+                socket.join(result[0].sockid);
+                console.log("USER FOUND : " + result[0].sockid);
+                predate = new Date();
+                console.log("PREDATE IS : " + predate);
+                setTimeout(function() {
+                    console.log('Time is up');
+                    socket.leave(result[0].sockid);
+                    postdate = new Date();
+                    console.log("POST DATE IS : " + postdate);
+                }, 60000);
+            }
         });
     });
-
-
- //CHAT MESSAGE FROM CLIENT
-
+    //CHAT MESSAGE FROM CLIENT
     socket.on('chat message', function(msg) {
         socket.to(msg.roomtgt).emit('chat message', msg);
+        //Place holder for store logic if needed
     });
-
- 
-
-
     //CLIENT DISCONNECTS
     socket.on('disconnect', function() {
-        io.emit('chat message', "disconnected");
+        console.log("disconnected: ", socket.client.id);
         connection.query('DELETE FROM lastknownuser WHERE sockid = ?', [socket.client.id], function(err, result) {
             if (err) throw err;
-            console.log("USER OFFLINE : " + result.id);
+            console.log("USER OFFLINE : " + result[0].id);
         });
         connection.query('UPDATE users SET ? WHERE sockid = ?', [{
             online: 0
         }, socket.client.id], function(err, result) {
             if (err) throw err;
-            console.log("USER OFFLINE : " + result.id);
+            console.log("USER OFFLINE : " + result[0].id);
         });
     });
-});
+    socket.on('reveal', function(msg) {
+        console.log("REAVEAl received from " + msg.name + " to: " + msg.roomtgt);
+        socket.to(msg.roomtgt).emit('chat message', {
+            text: msg.name + "HAS BEEN REVEALED"
+        });
+    });
+}); //END OF SOCKET CONNECTION
 //EXPRESS SERVER START.
 http.listen(3000, function() {
     console.log('listening on localhost:3000');
+});
+var killing_app = false;
+process.on('SIGINT', function() {
+    console.log("\n");
+    if (killing_app) {
+        console.log("[PROGRESS] Double SIGINT, Killing without cleanup!");
+        process.exit();
+    }
+    killing_app = true;
+    console.log("[PROGRESS] Gracefully shutting down from SIGINT (Ctrl+C)");
+    console.log("[PROGRESS] Disconnecting all sockets...");
+    console.log(Object.keys(io.engine.clients));
+    Object.keys(io.engine.clients).forEach(function(listener) {
+        console.log(listener);
+        io.sockets.connected[listener].disconnect();
+    });
+    console.log("[PROGRESS] Thank you bye bye <3 xoxo... please wait to exit");
+    setTimeout(function() {
+        console.log("Exit wait complete");
+        process.exit();
+    }, 10000);
 });
