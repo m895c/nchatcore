002d675d972446ac917c946fff6d0c9751695c0f
m895c <menon.ananth@gmail.com>
2015-03-27 08:27:50 +0530

    THis is for socket rooms

--- index.js
+++ index.js
@@ -2,120 +2,115 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mysql = require('mysql');
-
-
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'xcfc54123',
-  database : 'nchat'
+    host: 'localhost',
+    user: 'root',
+    password: 'xcfc54123',
+    database: 'nchat'
 });
-
-//app.get('/', function(req, res){
-//  res.send('<h1>NCORE HOST TEST </h1>  INIT ON:--*nCORe e92834n2323naskj32');
-//});
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+//Default Route
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
-
 //# TODO: setup static file serving correctly
-app.get('/css/main.css', function(req, res){
-  res.sendFile(__dirname + '/css/main.css');
+app.get('/css/main.css', function(req, res) {
+    res.sendFile(__dirname + '/css/main.css');
 });
-
-
 //SOCKET CONNECTION FROM CLIENT
-io.on('connection', function(socket){
-
-//CHAT MESSAGE FROM CLIENT
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
+io.on('connection', function(socket) {
+   
+    //CLIENT SENDS INFO MESSAGE WITH USER:
+    socket.on('info', function(msg) {
+        io.emit('infomessage', msg);
+        var info = {
+            name: msg.name,
+            age: msg.age,
+            sex: msg.sex,
+            target: msg.target,
+            token: msg.token,
+            online: 1,
+            sockid: socket.client.id
+        };
+        connection.query('INSERT IGNORE INTO users SET ?', info, function(err, result) {
+            if (err) throw err;
+            console.log("User inserted users :" + result.insertId);
+        });
+        connection.query('INSERT IGNORE INTO lastknownuser SET ?', info, function(err, result) {
+            if (err) throw err;
+            console.log("User inserted lastknown :" + result.insertId);
+        });
     });
-
-//CLIENT SENDS INFO MESSAGE WITH USER:
-    socket.on('info', function(msg){
-      io.emit('chat message', msg);
-
-      var info = {name: msg.name,
-                  age: msg.age,
-                  sex: msg.sex,
-                  target: msg.target,
-                  token: msg.token,
-                  online: 1,
-                  sockid: socket.client.id
-                 };
-
-      connection.query('INSERT IGNORE INTO users SET ?',info,function(err, result) {
-        if (err) throw err;
-
-        console.log("User inserted users :" + result.insertId);
-
-      });
-
-
-      connection.query('INSERT IGNORE INTO lastknownuser SET ?',info,function(err, result) {
-        if (err) throw err;
-
-        console.log("User inserted lastknown :" + result.insertId);
-
-      });
-    });
-
-//CLIENT SENDS SEARCH
-    socket.on('search', function(msg){
-      var search = {name: msg.name,
-                    age: msg.age,
-                    sex: msg.sex,
-                    target: msg.target,
-                    token: msg.token,
-                    sockid: socket.client.id
-                    };
-
-      //pseudocode
-      connection.query('SELECT sockid FROM lastknownuser where sex = ? ORDER BY sockid ASC LIMIT 1', [msg.target], function(err, result) {
-        if (err) throw err;
-
-        socket.room = search.token;
-
-        console.log("the search object is " + search.token);
-        console.log("the result is" + result[0]);
-
-        io.to(search.sockid).emit('matched', "room self: " + socket.room);
-
-        io.to(result[0].sockid).emit('matched', "room target: " + socket.room);
+    //CLIENT SENDS SEARCH
+    socket.on('search', function(msg) {
+        var search = {
+            name: msg.name,
+            age: msg.age,
+            sex: msg.sex,
+            target: msg.target,
+            token: msg.token,
+            sockid: socket.client.id
+        };
+        var agelolim = parseInt(msg.age) - 5;
+        var ageuplim = parseInt(msg.age) + 5;
+        //pseudocode
+        var result = [];
+        result[0] = null;
+        connection.query('SELECT sockid FROM lastknownuser WHERE sex = ? AND age BETWEEN ? AND ? ORDER BY sockid ASC LIMIT 1', [msg.target, agelolim, ageuplim], function(err, result) {
+            if (err) throw err;
+            console.log("the search object is " + search.token);
+            console.log("the result is" + result[0].sockid);
+            if (result[0] === null) {
+                io.to(search.sockid).emit('nomatch', "60");
+            } else  
 
 
-        console.log("USER FOUND : " + result[0].sockid);
-                    });
+            //socket.client(result[0].sockid).join(search.token);
 
+            io.to(search.sockid).emit('matched', result[0].sockid);
 
-    });
-//CLIENT DISCONNECTS
-    socket.on('disconnect', function(){
-      io.emit('chat message', "disconnected");
-      connection.query('DELETE FROM lastknownuser WHERE sockid = ?', [socket.client.id], function(err, result) {
-        if (err) throw err;
+            //io.to(search.sockid).emit('chat message', "SELF: " + search.sockid );
 
-        console.log("USER OFFLINE : " + result.id);
-      });
+            io.to(result[0].sockid).emit('matched', result[0].sockid);
 
-      connection.query('UPDATE users SET ? WHERE sockid = ?', [{online: 0}, socket.client.id], function(err, result) {
-        if (err) throw err;
+           // io.to(result[0].sockid).emit('chat message',"HOST " + result[0].sockid + "R" );
 
-        console.log("USER OFFLINE : " + result.id);
-      });
+            socket.join(result[0].sockid);
 
+            console.log("USER FOUND : " + result[0].sockid);
+            setTimeout(function () {
+            console.log('Time is up');
+            socket.leave(result[0].sockid);
+            },25000);
 
+        });
     });
 
 
+ //CHAT MESSAGE FROM CLIENT
+
+    socket.on('chat message', function(msg) {
+        socket.to(msg.roomtgt).emit('chat message', msg);
+    });
+
+ 
+
+
+    //CLIENT DISCONNECTS
+    socket.on('disconnect', function() {
+        io.emit('chat message', "disconnected");
+        connection.query('DELETE FROM lastknownuser WHERE sockid = ?', [socket.client.id], function(err, result) {
+            if (err) throw err;
+            console.log("USER OFFLINE : " + result.id);
+        });
+        connection.query('UPDATE users SET ? WHERE sockid = ?', [{
+            online: 0
+        }, socket.client.id], function(err, result) {
+            if (err) throw err;
+            console.log("USER OFFLINE : " + result.id);
+        });
+    });
 });
-
-
-
-
 //EXPRESS SERVER START.
-http.listen(3000, function(){
-  console.log('listening on *nCORe e92834n2323naskj32:3000');
+http.listen(3000, function() {
+    console.log('listening on localhost:3000');
 });
